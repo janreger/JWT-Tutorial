139b9cc1350d50f4e226ed558e73ddb064063515
janreger <janreger2005@gmail.com>
2025-10-03 22:28:24 +0200

    gitignore

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const corsorOptions = {credentials:true, original: process.env.URL || '*'};
+
+app.use(cors(corsOptions));
+app.use(json());
