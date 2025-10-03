b574575b0f361f584561017f1791773a081866a9
janreger <janreger2005@gmail.com>
2025-10-03 22:25:56 +0200

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
